4128922400c201603a7e18f38feb3ab0eaa3e502
z1qnezt <misha232dog@gmail.com>
2024-05-23 18:27:32 +0300

    end

---
+++ .github/workflows/test.sh
@@ -0,0 +1,9 @@
+root=./
+cmake --build $root/formatter_lib
+cmake --build $root/formatter_ex_lib 
+cmake --build $root/hello_world_application 
+cmake --build $root/solver_lib
+cmake --build $root/solver_application
+
+$root/hello_world_application/_build/main
+echo -e '1\n4\n3' | $root/solver_application/_build/main
