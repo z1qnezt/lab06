9069df83d4b150a57873dcf63939b9e7363c4942
z1qnezt <misha232dog@gmail.com>
2024-05-23 17:51:54 +0300

    end

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mihailerosenko/z1qnezt/workspace/projects/lab05_hw
+# Build directory: /Users/mihailerosenko/z1qnezt/workspace/projects/lab05_hw/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
